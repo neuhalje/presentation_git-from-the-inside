8937382cb7298c2f09ea09201015798466e04afe
Jens Neuhalfen <jens@neuhalfen.name>
2016-04-04 20:16:14 +0200

    Add initial Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,108 @@
 module.exports = function(grunt) {
+        "use strict";
 
-  // A very basic default task.
-  grunt.registerTask('default', 'Log some stuff.', function() {
-    grunt.log.write('Logging some stuff...').ok();
-  });
 
+        // Project configuration.
+        grunt.initConfig({
+                pkg: grunt.file.readJSON('package.json'),
+                watch: {
+                        files: '<config:lint.files>',
+                        tasks: 'default'
+                },
+
+                clean: {
+                        folder: [ "output", "tmp" ]
+                }, 
+                svgmin: {
+                        options: {
+                            plugins: [
+                                { removeViewBox: false },
+                                { removeEditorsNSData: true },
+                                { removeMetadata: true },
+                                { removeUselessStrokeAndFill: true },
+                                { removeEmptyContainers: true },
+                                { removeHiddenElems: true },
+                                { convertPathData: true },
+                                { collapseGroups: true },
+                                { removeDimensions: false },
+                                { removeXMLProcInst: false },
+                                //{ removeAttrs: { attrs: ['xmlns'] } }
+                            ]
+                        },
+                        dist: {
+                                files: [{
+                                        expand: true,
+                                        cwd: 'svg',
+                                        src: ['*.svg'],
+                                        dest: 'tmp/svg-svgmin'
+                                }]
+                        }
+                },
+                grunticon: {
+                        slides: {
+                                files: [{
+                                        expand: true,
+                                        cwd: 'tmp/svg-svgmin',
+                                        src: ['**/*.svg', '**/*.png'],
+                                        dest: "output"
+                                }],
+                                options: {
+
+                                        // CSS filenames
+                                        datasvgcss: "icons.data.svg.css",
+                                        datapngcss: "icons.data.png.css",
+                                        urlpngcss: "icons.fallback.css",
+
+                                        // preview HTML filename
+                                        previewhtml: "preview.html",
+
+                                        // grunticon loader code snippet filename
+                                        loadersnippet: "grunticon.loader.js",
+
+                                        // Include loader code for SVG markup embedding
+                                        enhanceSVG: true,
+
+                                        // Make markup embedding work across domains (if CSS hosted externally)
+                                        corsEmbed: false,
+
+                                        // folder name (within dest) for png output
+                                        pngfolder: "png",
+
+                                        // prefix for CSS classnames
+                                        cssprefix: ".icon-",
+
+                                        defaultWidth: "800px",
+                                        //defaultHeight: "200px",
+
+                                        // define vars that can be used in filenames if desirable, like foo.colors-primary-secondary.svg
+                                        colors: {
+                                                primary: "red",
+                                                secondary: "#666"
+                                        },
+
+                                        dynamicColorOnly: true,
+
+                                        // css file path prefix - this defaults to "/" and will be placed before the "dest" path when stylesheets are loaded.
+                                        // This allows root-relative referencing of the CSS. If you don't want a prefix path, set to to ""
+                                        cssbasepath: "/",
+                                        customselectors: {
+                                                "cat" : ["#el-gato"],
+                                                "gummy-bears-2" : ["nav li a.deadly-bears:before"]
+                                        },
+
+                                        // template: path.join( __dirname, "example", "default-css.hbs" ),
+                                        // previewTemplate: path.join( __dirname, "example", "preview-custom.hbs" ),
+
+                                        compressPNG: false
+
+                                }
+                        }
+                },
+        });
+
+        grunt.loadNpmTasks('grunt-contrib-clean');
+        grunt.loadNpmTasks('grunt-grunticon');
+        grunt.loadNpmTasks('grunt-svgmin');
+        grunt.registerTask('default', ['svgmin', 'grunticon:slides']);
 };
 
--- README.md
+++ README.md
@@ -9,3 +9,15 @@ This presentatation is used in  my _git from the inside_ talks.
 HOW TO USE IT
 ---------------
 TODO
+
+
+Building
+==========
+
+```sh
+# @pre: need grunt, npm
+
+npm install
+# ...
+grunt
+```
